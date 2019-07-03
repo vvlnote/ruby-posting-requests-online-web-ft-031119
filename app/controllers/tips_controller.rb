cb4e387770f38c20781d24bc4c2684e99b95413a
Kellye Greene <kellyecodes@gmail.com>
2019-07-03 18:47:14 -0400

    Fixes tips in controller

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -12,8 +12,8 @@ class TipsController < ApplicationController
   def index
     resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
       req.params['oauth_token'] = session[:token]
-      req.params['v'] = "20190702"
+      req.params['v'] = '20160201'
     end
-    @results = JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
+    @results = JSON.parse(resp.body)["response"]["tips"]["items"]
   end
 end
